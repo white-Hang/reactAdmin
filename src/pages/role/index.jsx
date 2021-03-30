bd00c7c35bd3f8fbd989e84232987796dec67407
wihte-hang <2291732295@qq.com>
2021-03-30 23:19:20 +0800

    fuwenben

--- src/pages/role/index.jsx
+++ src/pages/role/index.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
-
+import {
+    Card,
+    Button,
+    Table
+} from "antd"
 export default class Role extends Component {
     render() {
+        const title=(
+            <span>
+                <Button></Button>
+                <Button></Button>
+            </span>
+        )
         return (
-            <div>
-                Role
-            </div>
+            <Card title={title}>
+                
+            </Card>
         )
     }
 }
